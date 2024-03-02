acce1e2c5d886aed3eb83fa615f517d0aa778bb9
ramon <ramon.canoaparicio@gmail.com>
2024-03-02 15:03:29 +0100

    generate occurrences of chars in text

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,29 @@ fn get_word(s: &str) -> String {
     s.split(',').next().unwrap().to_string()
 }
 
+fn get_word_from(
+    text: &str,
+    start: usize,
+    words: &HashMap<String, Vec<u32>>,
+) -> Option<(usize, String)> {
+    let mut result: String = "".to_owned();
+    let chars = text.chars();
+    let mut i = start.clone();
+    for c in chars {
+        if i > 0 {
+            i -= 1;
+            continue;
+        }
+        let temp_result = result.clone() + &c.to_string();
+        if words.contains_key(&temp_result) {
+            result = temp_result.to_string();
+        } else {
+            return Some((start + result.chars().count() + 1, result));
+        }
+    }
+    Some((start + result.chars().count() + 1, result))
+}
+
 fn main() {
     let file = fs::File::open("./data/cedict.idx").unwrap();
     let buffered = BufReader::new(file);
@@ -46,5 +69,14 @@ fn main() {
         }
     }
     println!("Ok");
-    println!("你好 {:?}", result.get("你好"))
+    let text = "我比现在年轻十岁的时候，获得了一个游手好闲的职业，去乡间收集民间歌谣。那一年的整个夏天，我如同一只乱飞的麻雀，游荡在知了和阳光充斥的村舍田野。我喜欢喝农民那种带有苦味的茶水，他们的茶桶就放在田埂的树下，我毫无顾忌地拿起漆满茶垢的茶碗舀水喝，还把自己的水壶灌满，与田里干活的男人说上几句废话，在姑娘因我而起的窃窃私笑里扬长而去。我曾经和一位守着瓜田的老人聊了整整一个下午，这是我有生以来瓜吃得最多的一次，当我站起来告辞时，突然发现自己像个孕妇一样步履艰难了。然后我与一位当上了祖母的女人坐在门槛上，她编着草鞋为我唱了一支《十月怀胎》。我最喜欢的是傍晚来到时，坐在农民的屋前，看着他们将提上的井水泼在地上，压住蒸腾的尘土，夕阳的光芒在树梢上照射下来，拿一把他们递过来的扇子，尝尝他们和盐一样咸的咸菜，看看几个年轻女人，和男人们说着话。我头戴宽边草帽，脚上穿着拖鞋，一条毛巾挂在身后的皮带上，让它像尾巴似的拍打着我的屁股。我整日张大嘴巴打着呵欠，散漫地走在田间小道上，我的拖鞋吧哒吧哒，把那些小道弄得尘土飞扬，仿佛是车轮滚滚而过时的情景。我到处游荡，已经弄不清楚哪些村庄我曾经去过，哪些我没有去过。";
+    let mut i = 0;
+    let mut occurrences: HashMap<String, u32> = HashMap::new();
+    while i < text.chars().count() {
+        let r = get_word_from(&text, i, &result).unwrap();
+        let prev_ocurr = occurrences.get(&r.1).unwrap_or(&0);
+        occurrences.insert(r.1, prev_ocurr + 1);
+        i = r.0;
+    }
+    println!("{:?}", occurrences);
 }
